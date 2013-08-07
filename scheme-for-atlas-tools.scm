b02319c27283a08c4c1e3eec8795df25abb2cbf2
Hidenorry <hidenorry@gmail.com>
2013-08-07 21:21:26 +0900

    Modulized a tools.scm.

--- scheme-for-atlas-tools.scm
+++ scheme-for-atlas-tools.scm
@@ -1,10 +1,20 @@
 #!/usr/bin/env gosh
 
+(define-module scheme-for-atlas-tools
+  (use slib-wrapper)
+  (use srfi-13)
+  (export remove-extension
+          remove-str-extension
+          add-variables-name
+          f))
+(select-module scheme-for-atlas-tools)
+
 (define (remove-extension str ext)
     (let ((str-len (string-length str))
           (ext-len (string-length ext)))
       (if (string=? (string-copy str (- str-len ext-len) str-len) ext)
-          (string-copy str 0 (- str-len ext-len)) ; (string-copy str 0 4) <-- 4 is not contained.
+          (string-copy str 0 (- str-len ext-len))
+          ;; (string-copy str 0 4) <-- 4 is not contained.
           (error "unkown file" str))))
 (define (remove-str-extension str)
   (remove-extension str ".str"))
@@ -14,3 +24,6 @@
   (if (> (length lis) 1)
       (string-append device str (x->string var))
       device))
+
+(define (f num)
+  (x->number (string-trim-both (format #f "~10,5f" num))))
